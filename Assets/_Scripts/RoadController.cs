53e7d754f835d940b71fe6391b0818f9cfd22dd5
dpandya76 <dpandya76@gmail.com>
2016-03-02 19:04:22 -0500

    Added movement to tank and made background scrollable

---
+++ Assets/_Scripts/RoadController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public float speed=5;
+    private Transform _transform;
+
+	// Use this for initialization
+	void Start () {
+        this._transform = gameObject.GetComponent<Transform>();
+        this._Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 currentPosition = gameObject.GetComponent<Transform> ().position;
+		currentPosition.y -= speed;
+		gameObject.GetComponent<Transform> ().position = currentPosition;
+
+		// Check bottom boundary
+		if (currentPosition.y <= -480) {
+			this._Reset();
+		}
+	}
+
+	private void _Reset() {
+		Vector2 resetPosition = new Vector2 (0.0f, 480f);
+		gameObject.GetComponent<Transform> ().position = resetPosition;
+	}
+}
